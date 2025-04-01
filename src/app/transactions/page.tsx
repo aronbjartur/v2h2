568659807ed287e35fb4cb3d8effe933a933e564
Arnaldur <146443649+Add1Padd1@users.noreply.github.com>
2025-04-01 17:22:12 +0000

    Minor breyting

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,6 +1,6 @@
 import Header from '../components/Header/Header';
 import Transactions from '../components/Transactions/Transactions';
-
+// https://localhost:8000
 export default function Home() {
   return (
     <div>
